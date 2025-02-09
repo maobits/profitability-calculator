de2153960b8287652d7dd4c18800beed1e70050b
Mauricio Chara - Maobits LLC <admin@maobits.com>
2025-02-09 12:14:13 -0500

    Logical arithmetic calculation was implemented for arithmetic and logical calculation.

--- positionManager.js
+++ positionManager.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const API_URL = 'http://localhost:3000/api/yfinance/quote/';
+const API_URL = 'https://jpttrading-bitacora-server-1.onrender.com/api/yfinance/quote/';
 const API_KEY = '12345678-9abc-def0-1234-56789abcdef0';
 
 class PositionManager {
